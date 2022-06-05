9f7bb09137d7c3461ae170c45182925a432b64f9
1nekomata <73189703+1nekomata@users.noreply.github.com>
2022-06-05 19:19:23 +0200

    initial commit

---
+++ open_image.ps1
@@ -0,0 +1,17 @@
+$image_list = (Get-ChildItem -r | ForEach-Object { if ($_.PsIsContainer) { $_.FullName + "\" } else { $_.FullName } })
+
+Add-Type -AssemblyName System.Windows.Forms
+
+for ($i = 0; $i -lt $image_list.Count; $i++) {
+    Write-Output $image_list[$i]
+    if($image_list[$i] -Like "*.jpg" -or $image_list[$i] -Like "*.png" -or $image_list[$i] -Like "*.jpeg"){
+        Write-Output "Drücke Enter um das Bild zu zeigen"
+        Pause
+        Start-Process $image_list[$i] -WindowStyle maximized
+        Start-Sleep -Seconds 1
+        [System.Windows.Forms.SendKeys]::SendWait("{F11}")
+        Start-Sleep -Seconds 1
+        [System.Windows.Forms.SendKeys]::SendWait("^0")
+    }
+    Write-Output "not jpg"
+}
